91fbb3a23773927b06200e19ff10612c4eeb8dee
nicpmarks <104646616+nicpmarks@users.noreply.github.com>
2022-04-30 14:53:14 -0500

    final javascript

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Load year options 1-10 into select box on form load
 function loadSelect()
 {
     var yearsElement = document.getElementById("years");
@@ -9,9 +11,17 @@ function loadSelect()
     }
 }
 
+// Compute interest
 function compute()
 {
     var principal = document.getElementById("principal").value;
+
+    if (principal<=0) {
+        alert("Enter a positive number.");
+        document.getElementById("principal").focus();
+        return;
+    }
+
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
@@ -19,9 +29,15 @@ function compute()
     var total = parseInt(principal) + interest;
     
     var result = document.getElementById("result");
-    result.innerHTML = "";
+
+    // Use <mark> tag for highlighting
+    result.innerHTML = "<br/>If you deposit <mark>$" + principal + "</mark>,<br/>" +
+    "at an interest rate of <mark>" + rate + "%</mark>.<br/>" +
+    "You will receive an amount of <mark>$" + total + "</mark>,<br/>" +
+    "in the year <mark>" + year + "</mark>.";
 }
 
+// Update interest rate slider indicator 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
